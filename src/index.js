f09e6cf9cc52960e7b8b96bb1f1ac7db20b61e0d
buivantan01 <buivantan1234566@gmail.com>
2021-10-30 09:47:05 +0700

    Create sass for UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './sass/index.scss';
+
 import Layout from './components/Layout';
 ReactDOM.render(
   <React.StrictMode>
