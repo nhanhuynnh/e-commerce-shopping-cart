66e700d208afd09c17e048f70cc37ab6441f7c37
buivantan01 <buivantan1234566@gmail.com>
2021-11-05 11:12:00 +0700

    Update product

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,40 +1,29 @@
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { withRouter } from 'react-router';
 
 const ProductView = (props) => {
-  const product = props.product;
-
-  const [previewImg, setPreviewImg] = useState(product.image01);
-
-  return (
-    <div className='product'>
-      <div className='product__images'>
-        <div className='product__images__list'>
-          <div
-            className='product__images__list__item'
-            onClick={() => setPreviewImg(product.image01)}
-          >
-            <img src={product.image01} alt='' />
-          </div>
-          <div
-            className='product__images__list__item'
-            onClick={() => setPreviewImg(product.image02)}
-          >
-            <img src={product.image02} alt='' />
-          </div>
+    return (
+        <div className='product'>
+            <div className='product__images'>
+                <div className='product__images__list'>
+                    <div className='product__images__list__item'>
+                        <img src='' alt='' />
+                    </div>
+                    <div className='product__images__list__item'>
+                        <img src='' alt='' />
+                    </div>
+                </div>
+                <div className='product__images__main'>
+                    <img src='' alt='' />
+                </div>
+            </div>
         </div>
-        <div className='product__images__main'>
-          <img src={previewImg} alt='' />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 ProductView.propTypes = {
-  product: PropTypes.object.isRequired,
+    product: PropTypes.object.isRequired,
 };
 
 export default withRouter(ProductView);
