defe18061bb0ba274714c529eb6a52b80eac2a94
buivantan01 <buivantan1234566@gmail.com>
2021-11-07 13:26:22 +0700

    Added handle react Hooks for product view modal

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -1,12 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import ProductView from './ProductView';
 
 import Button from './Button';
+import productData from '../assets/fake-data/products';
 
 const ProductViewModal = () => {
+    const [product, setProduct] = useState(undefined);
+
+    useEffect(() => {
+        setProduct(productData.getProductBySlug());
+    }, []);
+
     return (
         <div>
             <div className='product-view__modal__content'>
-                <ProductView />
+                <ProductView product={product} />
                 <div className='product-view__modal__content__close'>
                     <Button>đóng</Button>
                 </div>
