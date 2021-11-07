b9cc7cb99ada8518f4741dbef39aae4ddd38b35c
buivantan01 <buivantan1234566@gmail.com>
2021-11-07 13:51:47 +0700

    Added handle redux for product view modal

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
 import ProductView from './ProductView';
 
 import Button from './Button';
+
+import { remove } from '../redux/product-modal/productModalSlice';
+
 import productData from '../assets/fake-data/products';
 
 const ProductViewModal = () => {
+    const productSlug = useSelector((state) => state.productModal.value);
+    const dispatch = useDispatch();
+
     const [product, setProduct] = useState(undefined);
 
     useEffect(() => {
-        setProduct(productData.getProductBySlug());
-    }, []);
+        setProduct(productData.getProductBySlug(productSlug));
+    }, [productSlug]);
 
     return (
-        <div>
+        <div className={`product-view__modal ${product === undefined ? '' : 'active'}`}>
             <div className='product-view__modal__content'>
                 <ProductView product={product} />
                 <div className='product-view__modal__content__close'>
-                    <Button>đóng</Button>
+                    <Button size='sm' onClick={() => dispatch(remove())}>
+                        đóng
+                    </Button>
                 </div>
             </div>
         </div>
