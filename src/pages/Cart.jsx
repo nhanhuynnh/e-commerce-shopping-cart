4685a6049732178cec8bda75981ab5a8bb66d9fa
buivantan01 <buivantan1234566@gmail.com>
2021-11-08 13:32:49 +0700

    Create cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Helmet from '../components/Helmet';
+import Button from '../components/Button';
+
+import numberWithCommas from '../utils/numberWithCommas';
 
 const Cart = () => {
-  return <div>Cart</div>;
+    return (
+        <Helmet title='Giỏ hàng'>
+            <div className='cart'>
+                <div className='cart__info'>
+                    <div className='cart__info__txt'>
+                        <p>Bạn đang có ? sản phẩm trong giỏ hàng</p>
+                        <div className='cart__info__txt__price'>
+                            <span>Thành tiền:</span> <span></span>
+                        </div>
+                    </div>
+                    <div className='cart__info__btn'>
+                        <Button size='block'>Đặt hàng</Button>
+                        <Link to='/catalog'>
+                            <Button size='block'>Tiếp tục mua hàng</Button>
+                        </Link>
+                    </div>
+                </div>
+                <div className='cart__list'></div>
+            </div>
+        </Helmet>
+    );
 };
 
 export default Cart;
