43426029932a13b3a54404a7873faa00a87cba5c
buivantan01 <buivantan1234566@gmail.com>
2021-11-20 09:25:22 +0700

    Refactor: add some handle cart item component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,21 +1,66 @@
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
+import { useDispatch } from 'react-redux';
+import { updateItem, removeItem } from '../redux/shopping-cart/cartItemsSlide';
+
+import { Link } from 'react-router-dom';
+
 const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const itemRef = useRef(null);
+
+  const [item, setItem] = useState(props.item);
+  const [quantity, setQuantity] = useState(props.item.quantity);
+
+  const updateQuantity = (opt) => {
+    if (opt === '+') {
+      dispatch(updateItem({ ...item, quantity: quantity + 1 }));
+    }
+    if (opt === '-') {
+      dispatch(
+        updateItem({ ...item, quantity: quantity - 1 === 0 ? 1 : quantity - 1 })
+      );
+    }
+  };
+
+  const removeCartItem = () => {
+    console.log('removeCartItem');
+    dispatch(removeItem(item));
+  };
+
   return (
-    <div className='cart__item'>
-      <div className='cart__item__image'></div>
+    <div className='cart__item' ref={itemRef}>
+      <div className='cart__item__image'>
+        <img src={item.product.image01} alt='' />
+      </div>
       <div className='cart__item__info'>
+        <div className='cart__item__info__name'>
+          <Link to={`/catalog/${item.slug}`}>
+            {`${item.product.title} - ${item.color} - ${item.size}`}
+          </Link>
+        </div>
+        <div className='cart__item__info__price'></div>
         <div className='cart__item__info__quantity'>
           <div className='product__info__item__quantity'>
-            <div className='product__info__item__quantity__btn'></div>
-            <div className='product__info__item__quantity__input'></div>
-            <div className='product__info__item__quantity__btn'>
+            <div
+              className='product__info__item__quantity__btn'
+              onClick={() => updateQuantity('-')}
+            >
+              <i className='bx bx-minus'></i>
+            </div>
+            <div className='product__info__item__quantity__input'>quantity</div>
+            <div
+              className='product__info__item__quantity__btn'
+              onClick={() => updateQuantity('+')}
+            >
               <i className='bx bx-plus'></i>
             </div>
           </div>
         </div>
         <div className='cart__item__del'>
-          <i className='bx bx-trash'></i>
+          <i className='bx bx-trash' onClick={() => removeCartItem()}></i>
         </div>
       </div>
     </div>
