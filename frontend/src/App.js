e72c00da8fd5f9e5505d69d5e565f3e302935143
buivantan01 <buivantan1234566@gmail.com>
2021-11-25 13:39:08 +0700

    Refactor: Create section Navigation and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+
+const Main = styled.main`
+  grid-area: main;
+`;
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
