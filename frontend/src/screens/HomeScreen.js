50c6736ec327b24a14dd79a1af217bedc169dffb
buivantan01 <buivantan1234566@gmail.com>
2021-11-26 20:20:39 +0700

    Create Home Screen for section screen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Collection from '../components/collections/Collection';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <Hero />
+      <Collection />
+    </>
+  );
+};
+
+export default HomeScreen;
